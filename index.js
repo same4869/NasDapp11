321ddac89940930b556d8131007c4aad4b489dfd
王迅 <wangxun@hujiang.com>
2018-05-28 18:49:26 +0800

    fix

--- index.js
+++ index.js
@@ -121,7 +121,38 @@ $(function () {
             console.log(html);
         }
         $('#tiles').append(html);
+        // resizeLi();
+
+        // applyLayout();
    }
+
+//    function resizeLi(){
+//         var $tiles = $('#tiles'),
+//         $handler = $('li', $tiles),
+//         $main = $('#main'),
+//         $window = $(window),
+//         $document = $(document),
+//         options = {
+//         autoResize: true, // This will auto-update the layout when the browser window is resized.
+//         container: $main, // Optional, used for some extra CSS styling
+//         offset: 20, // Optional, the distance between grid items
+//         itemWidth:280 // Optional, the width of a grid item
+//         };
+//         applyLayout();
+//    }
+
+//    function applyLayout() {
+//     $tiles.imagesLoaded(function() {
+//       // Destroy the old handler
+//       if ($handler.wookmarkInstance) {
+//         $handler.wookmarkInstance.clear();
+//       }
+
+//       // Create a new layout handler.
+//       $handler = $('li', $tiles);
+//       $handler.wookmark(options);
+//     });
+//   }
 
     $(".market_page .button.small.yellow").on("click", function(event) {
         var currentIndex = event.currentTarget.id;
